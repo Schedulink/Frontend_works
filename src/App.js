ce98428ff0ee528e7e4bf49d4ae8d0f3738a048b
Harini080922 <147321894+Harini080922@users.noreply.github.com>
2023-10-29 21:20:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import TimetableGeneration from './TimetableGeneration';
+
+
+export const App = () => {
+  return (
+    <div>
+      <TimetableGeneration/>
+      
+    </div>
+  );
+}
+export default App;
