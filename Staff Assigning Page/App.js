4d64f7c13a7e7878331ff0f68cb56311b777d632
Harini080922 <147321894+Harini080922@users.noreply.github.com>
2023-10-29 21:33:34 +0530

    Add files via upload

---
+++ Staff Assigning Page/App.js
@@ -0,0 +1,16 @@
+// src/App.js
+
+import React from 'react';
+import './CourseTable.css';
+import CourseTable from './CourseTable' 
+
+
+function App() {
+  return (
+    <div className="App">
+      <CourseTable/>
+      
+    </div>
+  );
+}
+export default App;
